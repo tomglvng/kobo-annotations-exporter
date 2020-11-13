eefc1b66c5e67a3bee43f83fc736a4d37d8845a0
Thomas Galvaing <thomas.galvaing@akeneo.com>
2020-11-13 22:04:32 +0100

    Add directory per authors + fix bug encoding + refacto write

--- services/txt_exporter.py
+++ services/txt_exporter.py
@@ -1,23 +1,25 @@
 from sanitize_filename import sanitize
+from pathlib import Path
 
 
 class TextExporter:
     @staticmethod
     def export(retrieved_annotations, directory) -> None:
         for author in retrieved_annotations:
+            author_directory = "{}/{}/".format(directory, sanitize(author))
+            Path(author_directory).mkdir(parents=True, exist_ok=True)
             books = retrieved_annotations[author]
             for book in books:
-                file_name = sanitize(author + '-' + book)
-                f = open(directory + file_name + '.txt', "a")
-                f.write('-' * 80)
+                book_file_name = "{}/{}.txt".format(author_directory, sanitize(book))
+                book_file = Path(book_file_name).open(mode="a", encoding="utf-8")
                 chapters = books[book]
                 for chapter in chapters:
-                    f.write('-' * 1 + chapter)
+                    book_file.write("\n\n{}\n".format(chapter))
                     annotations = chapters[chapter]
                     for annotation in annotations:
                         comment = ''
                         if annotation.comment is not None and annotation.comment:
-                            comment = '(' + annotation.comment + ') '
-                        last_update = '[' + annotation.last_update + '] '
-                        f.write('--' + last_update + comment + annotation.text)
-                f.close()
+                            comment = "({})".format(annotation.comment)
+                        last_update = "[{}]".format(annotation.last_update)
+                        book_file.write("{} - {}: {}\n\n".format(last_update, comment, annotation.text))
+                    book_file.write("\n")
--- services/word_exporter.py
+++ services/word_exporter.py
@@ -1,13 +1,17 @@
 from docx import Document
 from sanitize_filename import sanitize
+from pathlib import Path
 
 
 class WordExporter:
     @staticmethod
     def export(retrieved_annotations, directory) -> None:
         for author in retrieved_annotations:
+            author_directory = "{}/{}/".format(directory, sanitize(author))
+            Path(author_directory).mkdir(parents=True, exist_ok=True)
             books = retrieved_annotations[author]
             for book in books:
+                book_file_name = "{}/{}.docx".format(author_directory, sanitize(book))
                 document = Document()
                 document.add_heading(book, 0)
                 chapters = books[book]
@@ -19,6 +23,5 @@ class WordExporter:
                         if annotation.comment is not None and annotation.comment:
                             comment = '(' + annotation.comment + ') '
                         last_update = '[' + annotation.last_update + '] '
-                        p = document.add_paragraph(last_update + comment + annotation.text, style='Intense Quote')
-                file_name = sanitize(author + '-' + book)
-                document.save(directory + file_name + '.docx')
+                        document.add_paragraph(last_update + comment + annotation.text, style='Intense Quote')
+                document.save(book_file_name)
