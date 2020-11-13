6107b79d780c460686df027a00e79ed9fb9c2e12
Thomas Galvaing <tom.galvaing@gmail.com>
2020-11-14 00:29:28 +0100

    Refactor options management

--- kobo-annotations-exporter.py
+++ kobo-annotations-exporter.py
@@ -1,20 +1,43 @@
-from constants.messages import *
-from services.annotation_handler import AnnotationHandler
-from services.checkers import check_folder_exists
-from services.checkers import check_file_exists
 import argparse
+import os.path
+from services.annotation_handler import AnnotationHandler
+
+
+def check_file_existence(file_name: str) -> str:
+    if not os.path.isfile(file_name):
+        raise FileNotFoundError(file_name)
+    return file_name
+
+
+def check_folder_existence(directory_name: str) -> str:
+    if not os.path.isdir(directory_name):
+        raise NotADirectoryError(directory_name)
+    return directory_name if directory_name[-1] != '/' else directory_name[:-1]
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description=PROGRAM_DESCRIPTION)
+    parser = argparse.ArgumentParser(
+        description="Export annotations from Kobo sqlite database",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
     try:
-        parser.add_argument('sqlite', help=ARGUMENT_SQLITE_HELP, type=check_file_exists)
-        parser.add_argument('-d', '--directory', help=DIRECTORY_HELP, type=check_folder_exists)
-        parser.add_argument('-w', '--word', help=WORD_HELP, action="store_const", const="word")
-        parser.add_argument('-t', '--text', help=TEXT_HELP, action="store_const", const="text")
-        args = parser.parse_args()
+        parser.add_argument('sqlite',
+                            type=check_file_existence,
+                            help="Define the sqlite file location")
+        parser.add_argument('--format',
+                            '-f',
+                            type=str,
+                            choices={"console", "word", "text"},
+                            default="console",
+                            help="Specify the export format")
+        parser.add_argument('--directory',
+                            '-d',
+                            type=check_folder_existence,
+                            default="exports",
+                            help="Specify the export directory", )
 
-        annotation_handler = AnnotationHandler(args.sqlite, args.word, args.text, args.directory)
+        args = parser.parse_args()
+        annotation_handler = AnnotationHandler(args.sqlite, args.format, args.directory)
         annotation_handler.handle()
 
     except FileNotFoundError as not_found:
--- services/annotation_handler.py
+++ services/annotation_handler.py
@@ -1,29 +1,19 @@
 from services.annotation_retriever import AnnotationRetriever
 from services.console_exporter import ConsoleExporter
-from services.word_exporter import WordExporter
 from services.txt_exporter import TextExporter
+from services.word_exporter import WordExporter
 
 
 class AnnotationHandler:
-    def __init__(self, sqlite_file_name: str, word_format: str, text_format: str, export_directory: str) -> None:
+    def __init__(self, sqlite_file_name: str, export_format: str, export_directory: str) -> None:
         self.retriever = AnnotationRetriever(sqlite_file_name)
-
-        self.export_format = "console"
-
-        if word_format is not None:
-            self.export_format = word_format
-
-        if text_format is not None:
-            self.export_format = text_format
-
-        self.export_directory = "exports/" if export_directory is None else export_directory
+        self.export_format = export_format
+        self.export_directory = export_directory
 
     def handle(self) -> None:
-        annotations = self.retriever.retrieve()
-
-        if self.export_format == 'word':
-            WordExporter.export(annotations, self.export_directory)
-        elif self.export_format == 'text':
-            TextExporter.export(annotations, self.export_directory)
-        else:
-            ConsoleExporter.export(annotations)
+        exporter = {
+            "word": lambda a, d: WordExporter.export(a, d),
+            "text": lambda a, d: TextExporter.export(a, d),
+            "console": lambda a, d: ConsoleExporter.export(a),
+        }
+        exporter[self.export_format](self.retriever.retrieve(), self.export_directory)
